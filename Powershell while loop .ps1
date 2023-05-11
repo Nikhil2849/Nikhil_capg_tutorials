2b5a722501e76ae08453f5c792a2b15f2ed5f2e3
Nikhil2849 <132877451+Nikhil2849@users.noreply.github.com>
2023-05-11 17:23:21 +0530

    Add files via upload

---
+++ Powershell while loop .ps1
@@ -0,0 +1,30 @@
+﻿$i=0
+$j=0
+$k=0
+while($i -ne 10)
+{
+$filename = "Nikhil"+$i
+New-Item -Path $filename -ItemType directory
+$filetext = "Objext"+$i+".txt"
+New-Item -Path $filetext -ItemType File 
+cd C:\TEMP\powershellwhile\$filename
+while($j -ne 10){
+$filename1 = "Nikhil"+$j
+New-Item -Path $filename1 -ItemType directory
+$objectname ="Object"+$j+".txt"
+New-Item -path $objectname -ItemType File
+cd C:\TEMP\powershellwhile\$filename\$filename1
+while($k -ne 10){
+$objectname2 = "Object"+$k+".txt"
+New-Item -Path $objectname2 -ItemType File
+Remove-Item C:\TEMP\powershellwhile\$filename\$filename1\$objectname2
+cd C:\TEMP\powershellwhile\$filename\$filename1
+$k=$k+1
+}
+Remove-Item C:\TEMP\powershellwhile\$filename\$objectname
+cd C:\TEMP\powershellwhile\$filename
+$j=$j+1
+}
+cd C:\TEMP\powershellwhile
+$i=$i+1
+}
---
+++ powershell For.ps1
@@ -0,0 +1,29 @@
+﻿$i=0
+for($i;$i -lt 10;)
+{
+$filename = 'Nikhil'+$i
+New-Item -Path $filename -ItemType directory
+$filetext = 'Nikhil'+$i+'.txt'
+New-Item -Path $filetext -ItemType File
+cd C:\TEMP\PowershellFOR\$filename
+for($j=0;$j -lt 10;)
+{
+$filename0 ='Nikhil'+$j
+New-Item -Path $filename0 -ItemType directory
+$objectname ='Nikhilj'+$j+ '.txt'
+New-Item -path $objectname -ItemType File
+cd C:\TEMP\PowershellFOR\$filename\$filename0
+for($k=0; $k -lt 10;$k++)
+{
+$filename1 ='Nikhil'+$k+'.txt'
+New-Item -path $filename1 -ItemType File
+Remove-Item C:\TEMP\powershellFOR\$filename\$filename0\$filename1
+cd C:\TEMP\PowershellFOR\$filename\$filename0
+}
+Remove-Item C:\TEMP\powershellFOR\$filename\$objectname
+cd C:\TEMP\powershellFOR\$filename
+$j=$j+1
+}
+cd C:\TEMP\PowershellFOR
+$i=$i+1
+}
